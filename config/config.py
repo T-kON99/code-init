15f559db24263cdf0e880ac7a054ba7cbd64a7b8
Kenneth Anderson <eucliwood98@gmail.com>
2020-06-11 06:43:05 +0800

    Added resources dir name and the platform related urls

--- config/config.py
+++ config/config.py
@@ -2,6 +2,11 @@ import os
 import json
 
 ROOT = os.getcwd()
+RESOURCES_DIR_NAME = 'assets'
+PLATFORM_URLS = {
+    'projecteuler': 'https://projecteuler.net/',
+    'uhunt': 'https://onlinejudge.org/external/'
+}
 
 with open(f'{ROOT}/config/config.json') as file:
     setup: dict = json.load(file)
